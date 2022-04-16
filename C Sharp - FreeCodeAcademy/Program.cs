261fc92525bc57fe831c9296aac7e2eb3e2652fe
rupesh-biswas <rupeshbiswas1997@gmail.com>
2022-04-16 23:22:38 +0530

    first commit

--- C Sharp - FreeCodeAcademy/Program.cs
+++ C Sharp - FreeCodeAcademy/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once a man named " + characterName);
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
