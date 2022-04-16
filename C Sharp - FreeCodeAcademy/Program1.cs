ad5b509e22c9e1a8fa8f97ae86d9290a25bcbb6d
rupesh-biswas <rupeshbiswas1997@gmail.com>
2022-04-16 23:58:06 +0530

    update

---
+++ C Sharp - FreeCodeAcademy/Program1.cs
@@ -0,0 +1,18 @@
+﻿namespace Giraffe
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3;
+            bool isMale = true;
+            Console.WriteLine(30);
+
+
+            Console.ReadLine();
+        }
+    }
+}
