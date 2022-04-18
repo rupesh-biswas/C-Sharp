23e08c26dc3d477bddfac0a7c9fc58b62aaf687d
rupesh-biswas <rupeshbiswas1997@gmail.com>
2022-04-19 00:22:42 +0530

    day of the week

--- C Sharp - FreeCodeAcademy/Program1.cs
+++ C Sharp - FreeCodeAcademy/Program1.cs
@@ -4,38 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a day Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter an operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            } 
-            else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Operator");
-            }
-
+            Console.WriteLine(GetDay(num));
             Console.ReadLine();
         }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum)
+            {
+                case 0: 
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                default:
+                    dayName = "Invalid Input";
+                    break;
+            }
+
+            return dayName;        }
 
     }
 }
