810d1243682ef3e4c9b8ddc765298cee0b5e30d9
rupesh-biswas <rupeshbiswas1997@gmail.com>
2022-04-19 00:04:16 +0530

    calculator

--- C Sharp - FreeCodeAcademy/Program1.cs
+++ C Sharp - FreeCodeAcademy/Program1.cs
@@ -4,11 +4,38 @@
     {
         static void Main(string[] args)
         {
-            int num = 6;
-            num--;
-            Console.WriteLine( num );
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } 
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
 
             Console.ReadLine();
         }
+
     }
 }
